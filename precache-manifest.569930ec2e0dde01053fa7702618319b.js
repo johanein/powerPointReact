f6ba0d9a410c874074e25cbd980780fd0349c417
Albert <johanein@gmail.com>
2021-02-09 00:10:35 +0530

    Updates

---
+++ precache-manifest.569930ec2e0dde01053fa7702618319b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3044c40ad6357feb20abf7a8f3fe44",
+    "url": "/powerPointReact/index.html"
+  },
+  {
+    "revision": "97b798acb074693d6efc",
+    "url": "/powerPointReact/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "bf87e8a4faaed7130b60",
+    "url": "/powerPointReact/static/css/main.e76248d0.chunk.css"
+  },
+  {
+    "revision": "97b798acb074693d6efc",
+    "url": "/powerPointReact/static/js/2.42ca7f64.chunk.js"
+  },
+  {
+    "revision": "6785ea273d02b62d90dc9a069bd1cce8",
+    "url": "/powerPointReact/static/js/2.42ca7f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf87e8a4faaed7130b60",
+    "url": "/powerPointReact/static/js/main.4533b973.chunk.js"
+  },
+  {
+    "revision": "b73641e31c8f66ccaa8a",
+    "url": "/powerPointReact/static/js/runtime-main.ee49a864.js"
+  }
+]);
